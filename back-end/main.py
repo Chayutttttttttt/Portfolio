53cbc3eb8c6d548276b60242c078d27216ad281e
Folk <chayutphong951dikham@gmail.com>
2025-12-05 18:20:53 +0700

    update

--- back-end/main.py
+++ back-end/main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
+from sqlmodel import SQLModel,select,Field,Sequence
 from pydantic import BaseModel
 from typing import Union
 
-import os
-
+from pathlib import Path
 
 app = FastAPI()
 
@@ -19,14 +20,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def load_data_from_json(file_path):
+    try:
+        with open(file_path,"r",encoding="utf-8") as f:
+            data = json.load(f)
+            print(len(data))
+            return data
+    except FileNotFoundError:
+        print("File Not found")
+        return []
+
+SKILLS_DATA = load_data_from_json(Path(__file__).parent / "data" / "skills.json")
+IMG_DATA = Path(__file__).parent / "img"
+
 @app.get("/api/skills")
 def log_skills():
-    return [{"name": "Python", "level": "Advanced"},
-            {"name": "React", "level": "Intermediate"},
-            {"name": "FastAPI", "level": "Advanced"},]
+    return SKILLS_DATA
 
-@app.get("/api/my-img")
-def get_my_img():
-    return FileResponse("img/me.png")
+@app.get("/img/{filename}")
+def get_my_img(filename:str):
+    file_path = IMG_DATA / filename
+    print(f"Requesting file: {filename}")
+    if not file_path.exists():
+        return {"ERROR" : "Img not found"}
+    return FileResponse(file_path)
 
 app.mount("/",StaticFiles(directory="../personal-website/dist",html=True),name="fontend")
