c650cad6d883f1f796ea1714ad4a83b1ed11740a
Folk <chayutphong951dikham@gmail.com>
2025-12-05 23:51:47 +0700

    fix

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/Portfoilo/",
+  base: "/Portfolio/",
   plugins: [react(), tailwindcss()],
 })
