5567f9110da7ce34c7f297eae4eb365b617eebac
Folk <chayutphong951dikham@gmail.com>
2026-01-13 20:38:04 +0700

    fix

--- personal-website/src/Skill.jsx
+++ personal-website/src/Skill.jsx
@@ -1,22 +1,32 @@
-import { useState,useEffect } from "react";
+import { useState,useEffect,Suspense } from "react";
+import { useQuery } from "@tanstack/react-query";
 import "./skill.css";
 
 export default function skill() {    
     const [skills, setSkills] = useState([]);
     const [activeSkill, setActiveSkill] = useState(null);
-
-    useEffect(() => {
-        fetch("https://website-api-9hff.onrender.com/api/skills").then((res) => res.json())
-        .then((data) => setSkills(data))
-        .catch((err) => console.error("ERROR CAN'T LOAD DATA",err));
-    },[]);
-
+    const {data = [] ,isLoading ,Error} = useQuery({
+        queryKey : ["skills"],
+        queryFn : async () => {
+            const res = await fetch("https://website-api-9hff.onrender.com/api/skills");
+            if (!res.ok) throw new Error("Something went wrong...");
+            return res.json();
+        }
+    })
+    // useEffect(() => {
+    //     fetch("https://website-api-9hff.onrender.com/api/skills").then((res) => res.json())
+    //     .then((data) => setSkills(data))
+    //     .catch((err) => console.error("ERROR CAN'T LOAD DATA",err));
+    // },[]);
+    if (isLoading) return Loading();
+    if (Error) return <div>Error: {Eorror.message}</div>;
+    
     return (
         <>
             <div id="skill" className="skill">
-                <h2 className="skill-title">Skills & Experience</h2>
+                <Header/>
                 <div className="container">
-                    {skills.map((skill) =>
+                    {data.map(skill =>  (
                         <div key={skill.id}>
                             {activeSkill === skill.id && (
                                 <div className="popup-bg">
@@ -34,9 +44,26 @@ export default function skill() {
                                 {skill.name}
                             </div>
                         </div>
-                    )}
+                    ))}
                 </div>
             </div>
         </>
     );
+}
+
+function Loading() {
+    return (
+        <div className="skill">
+            <Header/>
+            <div className="loading"> Loading </div>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <h2 className="skill-title">Skills & Experience</h2>
+        </>
+    )
 }
--- personal-website/src/Welcome.jsx
+++ personal-website/src/Welcome.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect ,Suspense} from "react";
 import "./welcome.css";
 
 function Welcome() {
